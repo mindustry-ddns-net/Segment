484f0e27d159a07bfed71af70920acd5a89c6737
BnDLett <bridgesjacob@proton.me>
2025-03-08 21:18:37 -0500

    Initial Commit

---
+++ src/main/kotlin/net/ddns/mindustry/segment/Main.kt
@@ -0,0 +1,18 @@
+package net.ddns.mindustry.net.ddns.mindustry.segment
+
+import arc.util.CommandHandler
+import mindustry.mod.Plugin
+
+class Main : Plugin() {
+    override fun init() {
+
+    }
+
+    override fun registerClientCommands(handler: CommandHandler?) {
+
+    }
+
+    override fun registerServerCommands(handler: CommandHandler?) {
+
+    }
+}
